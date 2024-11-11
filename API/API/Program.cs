88e79d23c916366336aa33c5131edd8165c77696
DESKTOP-SVLI0O8\User <sokol3389@gmail.com>
2024-11-11 11:50:50 +0200

    edit appsettings.json, program.cs and included interface in service

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,4 +1,5 @@
-
+using API.Services;
+using API.Interfaces;
 namespace API
 {
     public class Program
@@ -6,8 +7,20 @@ namespace API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            ConfigurationManager configuration = builder.Configuration;
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var requestsSection = configuration.GetSection("Requests");
+
+            var sqlRequests = new Dictionary<string, string>();
+            foreach (var request in requestsSection.GetChildren())
+            {
+                sqlRequests[request.Key] = request.Value;
+            }
+
 
             // Add services to the container.
+            builder.Services.AddScoped<IAPIService, APIService>(provider => new APIService(connectionString, sqlRequests));
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +35,14 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors(options =>
+            {
+                options.AllowAnyOrigin()
+                       .AllowAnyHeader()
+                       .AllowAnyMethod();
+            });
+
 
             app.UseHttpsRedirection();
 
--- API/API/Services/APIService.cs
+++ API/API/Services/APIService.cs
@@ -1,6 +1,37 @@
-﻿namespace API.Services
+﻿using API.Interfaces;
+using API.Models;
+
+namespace API.Services
 {
-    public class APIService
+    public class APIService : IAPIService
     {
+        private readonly string _connectionString;
+        private readonly Dictionary<string, string> _sqlQueries;
+
+        public APIService(string connectionString, Dictionary<string, string> sqlQueries)
+        {
+            _connectionString = connectionString;
+            _sqlQueries = sqlQueries;
+        }
+
+        public void AddNote(NoteModel note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteNote(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<NoteModel>> GetAllNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateNote(UpdateNoteModel update)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
